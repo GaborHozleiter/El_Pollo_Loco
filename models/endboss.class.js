f5ce5d5e3f86ddc5144c4955e673f338f30476b7
GaborHozleiter <hozleitergabor@gmail.com>
2024-02-12 14:03:35 +0100

    added endboss

---
+++ models/endboss.class.js
@@ -0,0 +1,37 @@
+class Endboss extends MovebleObject {
+    height = 400;
+    width = 250;
+    y = 60;
+   
+
+    
+
+    IMAGES_ALERT = [
+        'img/4_enemie_boss_chicken/2_alert/G5.png',
+        'img/4_enemie_boss_chicken/2_alert/G6.png',
+        'img/4_enemie_boss_chicken/2_alert/G7.png',
+        'img/4_enemie_boss_chicken/2_alert/G8.png',
+        'img/4_enemie_boss_chicken/2_alert/G9.png',
+        'img/4_enemie_boss_chicken/2_alert/G10.png',
+        'img/4_enemie_boss_chicken/2_alert/G11.png',
+        'img/4_enemie_boss_chicken/2_alert/G12.png'
+    ]
+
+    
+
+
+    constructor(){
+        super().loadImage(this.IMAGES_ALERT[0]);
+        this.loadImages(this.IMAGES_ALERT);
+        this.x = 2200;
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_ALERT);
+        }, 200);
+        
+    }
+
+}
