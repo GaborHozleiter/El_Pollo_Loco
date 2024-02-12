4492815f9dd7530be11df3c7cefda80214496b04
GaborHozleiter <hozleitergabor@gmail.com>
2024-02-12 13:21:25 +0100

    optimize Character

--- models/character.class.js
+++ models/character.class.js
@@ -22,13 +22,13 @@ class Character extends MovebleObject{
 
     animate(){
     setInterval(() => {
-        if(this.world.keyboard.RIGHT){
+        if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){
             this.x += this.speed;
             this.otherDirection = false;
            
         }
 
-        if(this.world.keyboard.LEFT){
+        if(this.world.keyboard.LEFT && this.x > 0){
             this.x -= this.speed;
             this.otherDirection = true;
             
