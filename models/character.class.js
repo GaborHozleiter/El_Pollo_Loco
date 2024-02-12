877491f0f9c820421c327847ec48409285305a33
GaborHozleiter <hozleitergabor@gmail.com>
2024-02-12 13:28:01 +0100

    added walink sound

--- models/character.class.js
+++ models/character.class.js
@@ -5,6 +5,7 @@ class Character extends MovebleObject{
     y = 170;
     speed = 10;
     world;
+    walking_sound = new Audio('./audio/walk.mp3');
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -22,15 +23,18 @@ class Character extends MovebleObject{
 
     animate(){
     setInterval(() => {
+        this.walking_sound.pause();
         if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){
             this.x += this.speed;
             this.otherDirection = false;
+            this.walking_sound.play();
            
         }
 
         if(this.world.keyboard.LEFT && this.x > 0){
             this.x -= this.speed;
             this.otherDirection = true;
+            this.walking_sound.play();
             
         }
         this.world.camera_x = -this.x + 100;
