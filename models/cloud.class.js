95c68f6264012acdcdfd909bf696a1665c04a256
GaborHozleiter <hozleitergabor@gmail.com>
2024-02-12 09:43:24 +0100

    move clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -6,6 +6,14 @@ class Cloud extends MovebleObject{
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = 100 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate(){
+
+        setInterval(() => {
+            this.x -= 0.2;
+        }, 1000/60);
        
     }
 }
