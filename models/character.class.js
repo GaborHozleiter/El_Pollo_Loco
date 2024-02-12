d1192f4848e91fbfe8db2282abc6fa2570beaa56
GaborHozleiter <hozleitergabor@gmail.com>
2024-02-12 11:15:06 +0100

    added class keyboard and keyboard events

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovebleObject{
     width = 75;
     height = 300;
     y = 170;
+    world;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
