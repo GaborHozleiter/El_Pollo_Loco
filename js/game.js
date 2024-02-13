3d94c4475c1bf358eabe79c58c32bd302b21e2d2
GaborHozleiter <hozleitergabor@gmail.com>
2024-02-13 09:00:04 +0100

    added controlbuttons

--- js/game.js
+++ js/game.js
@@ -8,6 +8,8 @@ function startGame(){
     document.getElementById('startscreen').style.display = 'none';
     initLevel();
     init();
+    activateKeyboardControls();
+    activateButtonControls();
    
 }
 
@@ -18,58 +20,82 @@ function init(){
     console.log('My character is', world.character);
 }
 
-window.addEventListener("keydown", (e) => {
-    if(e.keyCode == 68){
-        keyboard.D = true;
-    }
+function activateKeyboardControls(){
+    window.addEventListener("keydown", (e) => {
+        if(e.keyCode == 68){
+            keyboard.D = true;
+        }
+        if(e.keyCode == 39 ){
+            keyboard.RIGHT = true;
+        }
+        if(e.keyCode == 37){
+            keyboard.LEFT = true;
+        }
+        if(e.keyCode == 38){
+            keyboard.UP = true;
+        }
+        if(e.keyCode == 40){
+            keyboard.DOWN = true;
+        }
+        if(e.keyCode == 32){
+            keyboard.SPACE = true;
+        }
+    });
+    
+    window.addEventListener("keyup", (e) => {
+        if(e.keyCode == 68){
+            keyboard.D = false;
+        }
+        if(e.keyCode == 39){
+            keyboard.RIGHT = false;
+        }
+        if(e.keyCode == 37){
+            keyboard.LEFT = false;
+        }
+        if(e.keyCode == 38){
+            keyboard.UP = false;
+        }
+        if(e.keyCode == 40){
+            keyboard.DOWN = false;
+        }
+        if(e.keyCode == 32){
+            keyboard.SPACE = false;
+        }
+    });
+}
 
-    if(e.keyCode == 39 ){
-        keyboard.RIGHT = true;
-    }
-
-    if(e.keyCode == 37){
+function activateButtonControls(){
+    document.getElementById('arrowLeft').addEventListener('touchstart', (e) => {
+        e.preventDefault();
         keyboard.LEFT = true;
-    }
-
-    if(e.keyCode == 38){
-        keyboard.UP = true;
-    }
-
-    if(e.keyCode == 40){
-        keyboard.DOWN = true;
-    }
-
-    if(e.keyCode == 32){
+    });
+    document.getElementById('arrowRight').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.RIGHT = true;
+    });
+    document.getElementById('jump').addEventListener('touchstart', (e) => {
+        e.preventDefault();
         keyboard.SPACE = true;
-    }
-
-   
-});
-
-window.addEventListener("keyup", (e) => {
-    if(e.keyCode == 68){
-        keyboard.D = false;
-    }
-
-    if(e.keyCode == 39){
-        keyboard.RIGHT = false;
-    }
-
-    if(e.keyCode == 37){
+    });
+    document.getElementById('throw').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.D = true;
+    });
+    document.getElementById('arrowLeft').addEventListener('touchend', (e) => {
+        e.preventDefault();
         keyboard.LEFT = false;
-    }
-
-    if(e.keyCode == 38){
-        keyboard.UP = false;
-    }
-
-    if(e.keyCode == 40){
-        keyboard.DOWN = false;
-    }
-
-    if(e.keyCode == 32){
+    });
+    document.getElementById('arrowRight').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        keyboard.RIGHT = false;
+    });
+    document.getElementById('jump').addEventListener('touchend', (e) => {
+        e.preventDefault();
         keyboard.SPACE = false;
-    }
+    });
+    document.getElementById('throw').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        keyboard.D = false;
+    });
+}
 
-     
-});
